57b8fdc021d9e5fe02e7bf754ba203c1ae797311
Maxine Fonua <maxinefonua@gmail.com>
2015-09-22 01:52:59 -0700

    messages added, sidebar removed

--- README.md
+++
@@ -1,7 +0,0 @@
-News Feed Eradicator
-====================
-
-This Chrome extension deletes your facebook news feed
-and replaces it with a nice quote.
-
-[Install from Chrome Web Store](https://chrome.google.com/webstore/detail/news-feed-eradicator-for/fjcldmjmjhkklehbacihaiopjklihlgg?hl=en)
--- eradicate.js
+++ eradicate.js
@@ -1,47 +1,47 @@
 var quoteList = [ 
 	{
 		quote: "I am not a prophet<br>but sometimes I have prophetic dreams.",
-		source: "Huey"
+		source: "Huey Freeman"
 	},
 	{
 		quote: "Jesus was black<br>Ronald Reagan was the Devil<br>and the government is lying about 9/11.<br>Thank you for your time and good night.",
-		source: "Huey"
+		source: "Huey Freeman"
 	},
 	{
 		quote: "Granddad, you can't tame the White Supremacist power structure with cheese!",
-		source: "Huey"
+		source: "Huey Freeman"
 	},
 	{
 		quote: "These people aren't worried about us.<br>They're not worried about anything.<br>They're rich. No matter what happens,<br>these people just keep applauding.",
-		source: "Huey"
+		source: "Huey Freeman"
 	},
 	{
 		quote: "Obi-Wan Kenobi once said:<br> Your eyes can deceive you, don't trust them.<br><br>Seems to be getting harder distinguishing reality<br>from the illusions that people make for us<br>or from the ones that we make for ourselves.<br>I don't know, maybe that's part of the plan.<br>To make me think I'm crazy.<br><br>It’s working.",
-		source: "Huey"
+		source: "Huey Freeman"
 	},
 	{
 		quote: "That night I dreamt of a blind swordsman.<br>He knows my every move, yet he cannot see.<br>As my mind fights to make sense of the impossible,<br>he is turning my sight into a liability.<br><br>He has no just cause to want my life.<br>There is no forethought, no logic in his actions.<br>This isn't just any swordsman.<br>This is the blind nigga samurai.",
-		source: "Huey"
+		source: "Huey Freeman"
 	},
 	{
 		quote: "Vision?<br>What do you know about my vision?<br>My vision would turn your world upside down,<br>tear asunder your illusions and send the sanctuary<br>of your own ignorance crashing down around you.<br><br>Now ask yourself: are you really ready<br>to see that vision?<br><br>...I want it in writing.",
-		source: "Huey"
+		source: "Huey Freeman"
 	},
 	{
 		quote: "Game recognize game, Granddad<br>and you lookin' kinda unfamiliar right now.",
-		source: "Riley"
+		source: "Riley Freeman"
 	},
 	{
 		quote: "First of all, imma live forever!<br>But if I do die,<br>I'm gon' smack God upside the head<br>and gon' tell him<br>to get me a grilled-cheese sandwich<br>and some tacos!",
-		source: "Riley"
+		source: "Riley Freeman"
 	},
 	{
 		quote: "Operation Black Steel - the mission to liberate Shabazz K. Milton-Berle - was aborted...<br>because I couldn't get a ride.",
-		source: "Huey"
+		source: "Huey Freeman"
 	},
 	{
 		quote: "Maybe there are forces in this universe<br>we don't understand<br><br>But I still believe <br>we make our own miracles<br><br>What makes your god<br>any less made up than his?",
-		source: "Huey"
+		source: "Huey Freeman"
 	},
 	{
 		quote: "I am the stone that the builder refused.",
@@ -49,83 +49,83 @@ var quoteList = [
 	},
 	{
 		quote: "How many times have I told you, you better not even <i>dream</i> about tellin' white folk the truth! You understand me?<br><br>Shoot, makin' white people riot. You better learn how to lie like me! I'm gonna find me a white man and lie to him right now!",
-		source: "Granddad"
+		source: "Robert Freeman"
 	},
 	{
-		quote: "How many times have I told you, you better not even <i>dream</i> about tellin' white folk the truth! You understand me?<br><br>Shoot, makin' white people riot. You better learn how to lie like me! I'm gonna find me a white man and lie to him right now!",
-		source: "Huey"
+		quote: "You do what you can to help black folks<br>and they make you wonder why you even <i>bother</i>.<br><br>But they're <i>our</i> people,<br>and we gotta love 'em regardless.",
+		source: "Huey Freeman"
 	},
 	{
-		quote: "How many times have I told you, you better not even <i>dream</i> about tellin' white folk the truth! You understand me?<br><br>Shoot, makin' white people riot. You better learn how to lie like me! I'm gonna find me a white man and lie to him right now!",
-		source: "Huey"
+		quote: "<i>But why would they lie to me?</i><br><br>Because the truth hurts, Jazmine.<br>The world is a hard and lonely place<br>and nobody gets anything for free.<br>And you want to know what else?<br>One day, you and everyone you know<br>is gonna die.",
+		source: "Huey Freeman"
 	},
 	{
-		quote: "How many times have I told you, you better not even <i>dream</i> about tellin' white folk the truth! You understand me?<br><br>Shoot, makin' white people riot. You better learn how to lie like me! I'm gonna find me a white man and lie to him right now!",
-		source: "Huey"
+		quote: "<i>We were just having a little fun.</i><br><br><b><i>Fun</i></b>?<br>Do I look like Charlie Brown?<br><br><i>...No.</i><br><br>Do I <b><i>look</i></b> like Charlie Brown?",
+		source: "Huey Freeman"
 	},
 	{
-		quote: "How many times have I told you, you better not even <i>dream</i> about tellin' white folk the truth! You understand me?<br><br>Shoot, makin' white people riot. You better learn how to lie like me! I'm gonna find me a white man and lie to him right now!",
-		source: "Huey"
+		quote: "<i>I ain't think they'd actually shoot back at us.</i><br><br>Well of course they're shooting back at us! You're robbing their store!<br><br><i>You can't assume that people are gonna shoot back at you. It was an unknown unknown.</i><br><br>Need I remind you that this has <b>nothing</b> to do<br>with our original plan.<br><br><i>Damn it, Huey! Robbery etiquette says you can't criticize a robbery plan during the actual robbery. You have to wait till the robbery's over.</i>",
+		source: "Gin Rummy and Huey Freeman"
 	},
 	{
-		quote: "How many times have I told you, you better not even <i>dream</i> about tellin' white folk the truth! You understand me?<br><br>Shoot, makin' white people riot. You better learn how to lie like me! I'm gonna find me a white man and lie to him right now!",
-		source: "Huey"
+		quote: "<i>I ain't think they'd actually shoot back at us.</i><br><br>Well of course they're shooting back at us! You're robbing their store!<br><br><i>You can't assume that people are gonna shoot back at you. It was an unknown unknown.</i><br><br>Need I remind you that this has <b>nothing</b> to do<br>with our original plan.<br><br><i>Damn it, Huey! Robbery etiquette says you can't criticize a robbery plan during the actual robbery. You have to wait till the robbery's over.</i>",
+		source: "Gin Rummy and Huey Freeman"
 	},
 	{
-		quote: "How many times have I told you, you better not even <i>dream</i> about tellin' white folk the truth! You understand me?<br><br>Shoot, makin' white people riot. You better learn how to lie like me! I'm gonna find me a white man and lie to him right now!",
-		source: "Huey"
+		quote: "<i>I ain't think they'd actually shoot back at us.</i><br><br>Well of course they're shooting back at us! You're robbing their store!<br><br><i>You can't assume that people are gonna shoot back at you. It was an unknown unknown.</i><br><br>Need I remind you that this has <b>nothing</b> to do<br>with our original plan.<br><br><i>Damn it, Huey! Robbery etiquette says you can't criticize a robbery plan during the actual robbery. You have to wait till the robbery's over.</i>",
+		source: "Gin Rummy and Huey Freeman"
 	},
 	{
-		quote: "How many times have I told you, you better not even <i>dream</i> about tellin' white folk the truth! You understand me?<br><br>Shoot, makin' white people riot. You better learn how to lie like me! I'm gonna find me a white man and lie to him right now!",
-		source: "Huey"
+		quote: "<i>I ain't think they'd actually shoot back at us.</i><br><br>Well of course they're shooting back at us! You're robbing their store!<br><br><i>You can't assume that people are gonna shoot back at you. It was an unknown unknown.</i><br><br>Need I remind you that this has <b>nothing</b> to do<br>with our original plan.<br><br><i>Damn it, Huey! Robbery etiquette says you can't criticize a robbery plan during the actual robbery. You have to wait till the robbery's over.</i>",
+		source: "Gin Rummy and Huey Freeman"
 	},
 	{
-		quote: "How many times have I told you, you better not even <i>dream</i> about tellin' white folk the truth! You understand me?<br><br>Shoot, makin' white people riot. You better learn how to lie like me! I'm gonna find me a white man and lie to him right now!",
-		source: "Huey"
+		quote: "<i>I ain't think they'd actually shoot back at us.</i><br><br>Well of course they're shooting back at us! You're robbing their store!<br><br><i>You can't assume that people are gonna shoot back at you. It was an unknown unknown.</i><br><br>Need I remind you that this has <b>nothing</b> to do<br>with our original plan.<br><br><i>Damn it, Huey! Robbery etiquette says you can't criticize a robbery plan during the actual robbery. You have to wait till the robbery's over.</i>",
+		source: "Gin Rummy and Huey Freeman"
 	},
 	{
-		quote: "How many times have I told you, you better not even <i>dream</i> about tellin' white folk the truth! You understand me?<br><br>Shoot, makin' white people riot. You better learn how to lie like me! I'm gonna find me a white man and lie to him right now!",
-		source: "Huey"
+		quote: "<i>I ain't think they'd actually shoot back at us.</i><br><br>Well of course they're shooting back at us! You're robbing their store!<br><br><i>You can't assume that people are gonna shoot back at you. It was an unknown unknown.</i><br><br>Need I remind you that this has <b>nothing</b> to do<br>with our original plan.<br><br><i>Damn it, Huey! Robbery etiquette says you can't criticize a robbery plan during the actual robbery. You have to wait till the robbery's over.</i>",
+		source: "Gin Rummy and Huey Freeman"
 	},
 	{
-		quote: "How many times have I told you, you better not even <i>dream</i> about tellin' white folk the truth! You understand me?<br><br>Shoot, makin' white people riot. You better learn how to lie like me! I'm gonna find me a white man and lie to him right now!",
-		source: "Huey"
+		quote: "<i>I ain't think they'd actually shoot back at us.</i><br><br>Well of course they're shooting back at us! You're robbing their store!<br><br><i>You can't assume that people are gonna shoot back at you. It was an unknown unknown.</i><br><br>Need I remind you that this has <b>nothing</b> to do<br>with our original plan.<br><br><i>Damn it, Huey! Robbery etiquette says you can't criticize a robbery plan during the actual robbery. You have to wait till the robbery's over.</i>",
+		source: "Gin Rummy and Huey Freeman"
 	},
 	{
-		quote: "How many times have I told you, you better not even <i>dream</i> about tellin' white folk the truth! You understand me?<br><br>Shoot, makin' white people riot. You better learn how to lie like me! I'm gonna find me a white man and lie to him right now!",
-		source: "Huey"
+		quote: "<i>I ain't think they'd actually shoot back at us.</i><br><br>Well of course they're shooting back at us! You're robbing their store!<br><br><i>You can't assume that people are gonna shoot back at you. It was an unknown unknown.</i><br><br>Need I remind you that this has <b>nothing</b> to do<br>with our original plan.<br><br><i>Damn it, Huey! Robbery etiquette says you can't criticize a robbery plan during the actual robbery. You have to wait till the robbery's over.</i>",
+		source: "Gin Rummy and Huey Freeman"
 	},
 	{
-		quote: "How many times have I told you, you better not even <i>dream</i> about tellin' white folk the truth! You understand me?<br><br>Shoot, makin' white people riot. You better learn how to lie like me! I'm gonna find me a white man and lie to him right now!",
-		source: "Huey"
+		quote: "<i>I ain't think they'd actually shoot back at us.</i><br><br>Well of course they're shooting back at us! You're robbing their store!<br><br><i>You can't assume that people are gonna shoot back at you. It was an unknown unknown.</i><br><br>Need I remind you that this has <b>nothing</b> to do<br>with our original plan.<br><br><i>Damn it, Huey! Robbery etiquette says you can't criticize a robbery plan during the actual robbery. You have to wait till the robbery's over.</i>",
+		source: "Gin Rummy and Huey Freeman"
 	},
 	{
-		quote: "How many times have I told you, you better not even <i>dream</i> about tellin' white folk the truth! You understand me?<br><br>Shoot, makin' white people riot. You better learn how to lie like me! I'm gonna find me a white man and lie to him right now!",
-		source: "Huey"
-    },
-    {
-		quote: "How many times have I told you, you better not even <i>dream</i> about tellin' white folk the truth! You understand me?<br><br>Shoot, makin' white people riot. You better learn how to lie like me! I'm gonna find me a white man and lie to him right now!",
-		source: "Huey"
+		quote: "<i>I ain't think they'd actually shoot back at us.</i><br><br>Well of course they're shooting back at us! You're robbing their store!<br><br><i>You can't assume that people are gonna shoot back at you. It was an unknown unknown.</i><br><br>Need I remind you that this has <b>nothing</b> to do<br>with our original plan.<br><br><i>Damn it, Huey! Robbery etiquette says you can't criticize a robbery plan during the actual robbery. You have to wait till the robbery's over.</i>",
+		source: "Gin Rummy and Huey Freeman"
 	},
 	{
-		quote: "How many times have I told you, you better not even <i>dream</i> about tellin' white folk the truth! You understand me?<br><br>Shoot, makin' white people riot. You better learn how to lie like me! I'm gonna find me a white man and lie to him right now!",
-		source: "Huey"
-    },
-	{
-		quote: "How many times have I told you, you better not even <i>dream</i> about tellin' white folk the truth! You understand me?<br><br>Shoot, makin' white people riot. You better learn how to lie like me! I'm gonna find me a white man and lie to him right now!",
-		source: "Huey"
-    },
-    {
-		quote: "How many times have I told you, you better not even <i>dream</i> about tellin' white folk the truth! You understand me?<br><br>Shoot, makin' white people riot. You better learn how to lie like me! I'm gonna find me a white man and lie to him right now!",
-		source: "Huey"
+		quote: "<i>I ain't think they'd actually shoot back at us.</i><br><br>Well of course they're shooting back at us! You're robbing their store!<br><br><i>You can't assume that people are gonna shoot back at you. It was an unknown unknown.</i><br><br>Need I remind you that this has <b>nothing</b> to do<br>with our original plan.<br><br><i>Damn it, Huey! Robbery etiquette says you can't criticize a robbery plan during the actual robbery. You have to wait till the robbery's over.</i>",
+		source: "Gin Rummy and Huey Freeman"
 	},
 	{
-		quote: "How many times have I told you, you better not even <i>dream</i> about tellin' white folk the truth! You understand me?<br><br>Shoot, makin' white people riot. You better learn how to lie like me! I'm gonna find me a white man and lie to him right now!",
-		source: "Huey"
+		quote: "<i>I ain't think they'd actually shoot back at us.</i><br><br>Well of course they're shooting back at us! You're robbing their store!<br><br><i>You can't assume that people are gonna shoot back at you. It was an unknown unknown.</i><br><br>Need I remind you that this has <b>nothing</b> to do<br>with our original plan.<br><br><i>Damn it, Huey! Robbery etiquette says you can't criticize a robbery plan during the actual robbery. You have to wait till the robbery's over.</i>",
+		source: "Gin Rummy and Huey Freeman"
 	},
 	{
-		quote: "How many times have I told you, you better not even <i>dream</i> about tellin' white folk the truth! You understand me?<br><br>Shoot, makin' white people riot. You better learn how to lie like me! I'm gonna find me a white man and lie to him right now!",
-		source: "Huey"
+		quote: "<i>I ain't think they'd actually shoot back at us.</i><br><br>Well of course they're shooting back at us! You're robbing their store!<br><br><i>You can't assume that people are gonna shoot back at you. It was an unknown unknown.</i><br><br>Need I remind you that this has <b>nothing</b> to do<br>with our original plan.<br><br><i>Damn it, Huey! Robbery etiquette says you can't criticize a robbery plan during the actual robbery. You have to wait till the robbery's over.</i>",
+		source: "Gin Rummy and Huey Freeman"
+	},
+	{
+		quote: "<i>I ain't think they'd actually shoot back at us.</i><br><br>Well of course they're shooting back at us! You're robbing their store!<br><br><i>You can't assume that people are gonna shoot back at you. It was an unknown unknown.</i><br><br>Need I remind you that this has <b>nothing</b> to do<br>with our original plan.<br><br><i>Damn it, Huey! Robbery etiquette says you can't criticize a robbery plan during the actual robbery. You have to wait till the robbery's over.</i>",
+		source: "Gin Rummy and Huey Freeman"
+	},
+	{
+		quote: "<i>I ain't think they'd actually shoot back at us.</i><br><br>Well of course they're shooting back at us! You're robbing their store!<br><br><i>You can't assume that people are gonna shoot back at you. It was an unknown unknown.</i><br><br>Need I remind you that this has <b>nothing</b> to do<br>with our original plan.<br><br><i>Damn it, Huey! Robbery etiquette says you can't criticize a robbery plan during the actual robbery. You have to wait till the robbery's over.</i>",
+		source: "Gin Rummy and Huey Freeman"
+	},
+	{
+		quote: "<i>I ain't think they'd actually shoot back at us.</i><br><br>Well of course they're shooting back at us! You're robbing their store!<br><br><i>You can't assume that people are gonna shoot back at you. It was an unknown unknown.</i><br><br>Need I remind you that this has <b>nothing</b> to do<br>with our original plan.<br><br><i>Damn it, Huey! Robbery etiquette says you can't criticize a robbery plan during the actual robbery. You have to wait till the robbery's over.</i>",
+		source: "Gin Rummy and Huey Freeman"
 	}
 ];
 
@@ -163,7 +163,7 @@ var extensionURL = function(relativeURL){
     }
 }
 
-fbLink = $("<a href='javascript:;'>.</a>")
+fbLink = $("<a href='javascript:;'></a>")
     .addClass('nfe-info-link')
     .on('click', function(){
       var handleClose = function() {
@@ -195,11 +195,9 @@ setInterval(function(){
 	$("div#pagelet_home_stream").replaceWith(quoteDiv);
 	$("div[id^='topnews_main_stream']").replaceWith(quoteDiv);
 
-    // Delete the ticker
-    $("div#pagelet_ticker").remove();
+    // Delete the sidebar
+    $("div#pagelet_sidebar").remove();
 
-    // Delete the trending box
-    $("div#pagelet_trending_tags_and_topics").remove();
 }, 1000);
 
 
